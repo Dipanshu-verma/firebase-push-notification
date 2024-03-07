7743d56856beef0e071a27efb6bbf7ea96fb00c4
Dipanshu-verma <vermadipanshu444@gmail.com>
2024-03-07 14:11:15 +0530

    permission

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 // App.js
-import logo from './assets/sparky-dash-high-five.gif';
-import './App.css';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { ganerateToken } from './notification/firebase';
-import { getMessaging, onMessage } from 'firebase/messaging';
+import logo from "./assets/sparky-dash-high-five.gif";
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import { ganerateToken } from "./notification/firebase";
+import { getMessaging, onMessage } from "firebase/messaging";
 
 function App() {
   const [notificationData, setNotificationData] = useState({
-    title: '',
-    body: '',
-    imageUrl: '',
-  
+    title: "",
+    body: "",
+    imageUrl: "",
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setNotificationData(prevState => ({
+    setNotificationData((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
 
@@ -27,36 +26,64 @@ function App() {
     ganerateToken(notificationData);
   };
 
+
+  
+  async function permision(){
+
+    const permision =  await Notification.requestPermission();
+    if (permision === "granted") {
+      console.log("granted");
+    } else {
+      console.log("not granted");
+    }
+  }
+  
   useEffect(() => {
+
+    permision();
     onMessage(getMessaging, (payload) => {
       console.log(payload);
-      
     });
+
   }, []);
 
-const handlepermision = async()=>{
-  
- const permision = await  Notification.requestPermission();
- if(permision === 'granted'){
-  console.log("granted");
- }else{
-  console.log("not granted");
- }
-
-}
+ 
 
   return (
     <div className="App">
-     <img src={logo} className="app-logo" alt="logo"  width="400px" height="500px"/>
+      <img
+        src={logo}
+        className="app-logo"
+        alt="logo"
+        width="400px"
+        height="500px"
+      />
       <form onSubmit={handleSubmit}>
-        <input type="text" name="title" placeholder="Title" value={notificationData.title} onChange={handleChange} />
-        <input type="text" name="body" placeholder="Body" value={notificationData.body} onChange={handleChange} />
-        <input type="text" name="imageUrl" placeholder="Image URL" value={notificationData.imageUrl} onChange={handleChange} />
-        
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          value={notificationData.title}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="body"
+          placeholder="Body"
+          value={notificationData.body}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="imageUrl"
+          placeholder="Image URL"
+          value={notificationData.imageUrl}
+          onChange={handleChange}
+        />
+
         <button type="submit">Send Notification</button>
-        <button onClick={handlepermision}>allow notification</button>
+        
       </form>
-     
     </div>
   );
 }
--- src/notification/firebase.js
+++ src/notification/firebase.js
@@ -1,8 +1,6 @@
-
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken } from "firebase/messaging";
 
- 
 const firebaseConfig = {
   apiKey: "AIzaSyB1Dny2lUB65CXk_FCF7vwG8mq_OwI60ys",
   authDomain: "push-edbf5.firebaseapp.com",
@@ -12,77 +10,49 @@ const firebaseConfig = {
   appId: "1:992368646994:web:116eb7eac561ee0ec1f88b",
 };
 
-
- 
 const app = initializeApp(firebaseConfig);
 
- 
 const messaging = getMessaging(app);
 
-
- 
-
-
 export const ganerateToken = async (notificationData) => {
-   
   let permission = Notification.permission;
 
-   
   if (permission !== "granted") {
     permission = await Notification.requestPermission();
     if (permission !== "granted") {
-     
       console.log("Notification permission denied.");
       return;
     }
   }
-
 
   const token = await getToken(messaging, {
     vapidKey:
       "BJg4xH49f04vbw4Ssw1-NPfST1b6IhL3LJDuqLV2_VZDac8icey0O5b0A7Tgb-N58VdBYh52dvwpsbyl0KS09Ro",
   });
 
- 
-
-
-let message = {
-  to:token,
-  notification:{
-    title: notificationData.title,
-    body: notificationData.body,
-    
-  }
-}
- 
-
-
-try {
-  let res = await fetch("https://fcm.googleapis.com/fcm/send", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `key= AAAA5w3H21I:APA91bGybyMHxhFtgrmQW0hQDZkrMELWndMhHQuoVjqwAHEkR4qfJcmmNVRVIh5D9Lh9I1ZILTVmnfZcy3asEh5egO62e2k7QzQSRPR7gg5ZTBfSOvgwYDswgIqSnQwWZ04HnwtyB94J` 
+  let message = {
+    to: token,
+    notification: {
+      title: notificationData.title,
+      body: notificationData.body,
     },
+  };
 
-    
-    body: JSON.stringify(message)
+  try {
+    let res = await fetch("https://fcm.googleapis.com/fcm/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `key= AAAA5w3H21I:APA91bGybyMHxhFtgrmQW0hQDZkrMELWndMhHQuoVjqwAHEkR4qfJcmmNVRVIh5D9Lh9I1ZILTVmnfZcy3asEh5egO62e2k7QzQSRPR7gg5ZTBfSOvgwYDswgIqSnQwWZ04HnwtyB94J`,
+      },
 
-    
-  });
+      body: JSON.stringify(message),
+    });
 
-
- 
-
- 
-  // Handle response
-} catch (error) {
-  console.error(error);
-}
-
-
-
-
+    // Handle response
+  } catch (error) {
+    console.error(error);
+  }
 
   try {
     let res = await fetch("https://push-notification-yht6.onrender.com/token", {
@@ -96,10 +66,4 @@ try {
   } catch (error) {
     console.error(error);
   }
-
-
-
 };
-
-
-
